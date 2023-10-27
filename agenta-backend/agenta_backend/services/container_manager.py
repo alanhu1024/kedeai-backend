f22a05d34fecc24f119f92f14eb6287ef15ed879
guolianwei <292672967@qq.com>
2023-10-27 11:41:22 +0800

    增加日志时间的输出过程。

--- agenta-backend/agenta_backend/services/container_manager.py
+++ agenta-backend/agenta_backend/services/container_manager.py
@@ -21,7 +21,7 @@ client = docker.from_env()
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
 
 def build_image_job(
         app_name: str,
